79cde91cc475456c663fcbc10be8cf0cab29e858
LeeJaeSeok1 <98381653+LeeJaeSeok1@users.noreply.github.com>
2022-06-09 14:47:29 +0900

    커밋

--- src/index.js
+++ src/index.js
@@ -1,10 +1,91 @@
-class Site {}
+class Site {
+    constructor() {
+    this.boards = [];
+    }
+    addBoard(board) {
+        for(let i=0; i < this.boards.length; i++) {
+            if (this.boards[i].name === board.name) {
+                throw new Error()
+            }
+        }
+        board.flag = true;
+        this.boards.push(board);
+    }
+    
+    findBoardByName(boardname) {
+        for(let i = 0; i < this.boards.length; i++) {
+            if(this.boards[i].name === boardname) {
+                return this.boards[i];
+            }
+        }
+        throw new Error()
+    }
+}
 
-class Board {}
 
-class Article {}
+class Board {
+    constructor(name) {
+        if(!name) {
+            throw new Error();
+        }
+        this.name = name;
+        this.flag = false;
+        this.article = [];
+    }
 
-class Comment {}
+    publish(article) {
+        if(this.flag !== true ){
+            throw new Error();
+        }
+        article.flag = true;
+        article.createdDate = new Date().toISOString()
+        article.id = `${this.name}-${Math.random()}`
+        this.article.push(article);
+    }
+
+    getAllArticles() {
+        return this.article;
+    }
+}
+
+
+
+class Article {
+    constructor({subject, content, author}) {
+        if(!subject||!content||!author){
+            throw new Error();
+        }
+        this.subject = subject;
+        this.content = content;
+        this.author = author;
+        this.id;
+        this.createdDate;
+        this.flag = false;
+        this.comment = [];
+    }
+    
+    reply(comment) {
+        if(this.flag === false){
+            throw new Error();
+        }
+        comment.createdDate = new Date().toISOString();
+        this.comment.push(comment)
+    }
+    getAllComments() {
+        return this.comment;
+    }
+}
+
+class Comment {
+    constructor({content, author}){
+        if(!content||!author){
+            throw new Error();
+        }
+        this.content = content;
+        this.author = author;
+        this.createdDate;
+    }
+}
 
 module.exports = {
     Site,
@@ -12,3 +93,5 @@ module.exports = {
     Article,
     Comment,
 };
+
+
